663186ebc894afad356f2d5a362c3ca423412159
Bernardo <moonbe77@gmail.com>
2020-06-23 19:46:02 +1000

    removed all the trash from server side

--- pages/api/product/[id].ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "GET") {
-    res.status(500).json({ message: "HTTP method not accepted" });
-  }
-  res.statusCode = 200;
-  res.json({ prodId: req.query.id , method: req.method });
-};
